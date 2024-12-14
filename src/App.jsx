7a7d72dcf8f8116d721e670ecf608d54e82494f5
alireza akbarzadeh <devtools95@gmail.com>
2024-12-14 12:20:48 +0330

    feat: setup award project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <div>
+
+   <h3 className="award winning app font-circular-web">Award</h3>
+      </div>
+  )
+}
+
+export default App
